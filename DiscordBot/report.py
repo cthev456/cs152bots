51bc6d372beea7a807cf5f27f044ecb45c93183a
cthev456 <cthev456@gmail.com>
2023-05-31 17:41:12 -0700

    Added multiple message linking for 'other' spam type

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -8,6 +8,8 @@ class State(Enum):
     MESSAGE_IDENTIFIED = auto()
     OFFENDER_STATUS_IDENTIFIED = auto()
     AWAITING_SPAM_TYPE = auto()
+    AWAITING_OTHER_SPAM_TYPE = auto()
+    AWAITING_MULTIPLE_MESSAGES = auto()
     RECEIVED_SPAM_TYPE = auto()
     REPORT_COMPLETE = auto()
     MOD_COMPLETE = auto()
@@ -43,6 +45,7 @@ class Report:
         self.client = client
         self.channel = None
         self.message = None
+        self.other_message_chain = None
         self.report_type = None
         self.repeat_offender = None
         self.spam_type = None
@@ -66,7 +69,7 @@ class Report:
         
         if self.state == State.REPORT_START:
             reply =  "Thank you for starting the reporting process. "
-            reply += "Say `help` at any time for more information.\n\n"
+            reply += "Say `help` at any time for more information. At any point, you can say 'cancel' to cancel the entire report. \n\n"
             reply += "Please copy paste the link to the message you want to report.\n"
             reply += "You can obtain this link by right-clicking the message and clicking `Copy Message Link`."
             self.state = State.AWAITING_MESSAGE
@@ -136,7 +139,7 @@ class Report:
                     "1. The message is an unwanted advertisement that has nothing to do with the server; type 'advertising' \n"
                     "2. Unwanted invites to other servers; type 'invites' \n"
                     "3. The message contains a suspicious, abusive, or NSFW link; type 'links' \n"
-                    "4. Other; type 'other' \n"]
+                    "4. Other such as harrassment spam involving multiple messages; type 'other' \n"]
 
         if self.state == State.AWAITING_SPAM_TYPE:
             if message.content not in [SpamType.ADVERTISING, SpamType.INVITES, SpamType.MALICIOUS_LINKS, SpamType.OTHER]:
@@ -144,12 +147,52 @@ class Report:
                 report_reply += "1. The message is an unwanted advertisement that has nothing to do with the server; type 'advertising' \n"
                 report_reply += "2. Unwanted invites to other servers; type 'invites' \n"
                 report_reply += "3. The message contains a suspicious, abusive, or NSFW link; type 'links' \n"
-                report_reply += "4. Other; type 'other' \n"
+                report_reply += "4. Other such as harrassment spam involving multiple messages; type 'other' \n"
                 return [report_reply]
             self.spam_type = message.content
+            if self.spam_type == SpamType.OTHER:
+                self.state = State.AWAITING_OTHER_SPAM_TYPE
+                return ["For 'other' spam, would you like to link multiple offending spam messages? Please reply with 'yes' or 'no.' \n"]
             self.state = State.RECEIVED_SPAM_TYPE
             return ["I have noted that the spam type is " + self.spam_type + ". Would you like to block this user and any future accounts they make? Reply with 'yes' or 'no.' \n"]
         
+        if self.state == State.AWAITING_OTHER_SPAM_TYPE:
+            report_reply = ''
+            if message.content not in ['yes', 'no']:
+                report_reply += "I'm sorry, I didn't understand that. Would you like to link multiple offending spam messages? Please reply with 'yes' or 'no.' \n"
+                return [report_reply]
+            if message.content == 'yes':
+                self.state = State.AWAITING_MULTIPLE_MESSAGES
+                self.other_message_chain = self.message.content
+                return ["Please reply with a link to the offending messages. Please note, this must be from the same offender.\n"]
+            else:
+                self.state = State.RECEIVED_SPAM_TYPE
+            return ["I have noted that the spam type is " + self.spam_type + ". Would you like to block this user and any future accounts they make? Reply with 'yes' or 'no.' \n"]
+        
+        if self.state == State.AWAITING_MULTIPLE_MESSAGES:
+            add_msg = None
+            if message.content.lower() == 'done':
+                self.state = State.RECEIVED_SPAM_TYPE
+                return ["I have noted that the spam type is " + self.spam_type + ". Would you like to block this user and any future accounts they make? Reply with 'yes' or 'no.' \n"]
+            # Parse out the three ID strings from the message link
+            m = re.search('/(\d+)/(\d+)/(\d+)', message.content)
+            if not m:
+                return ["I'm sorry, I couldn't read that link. Please try again or say `done` to proceed with finishing the report."]
+            guild = self.client.get_guild(int(m.group(1)))
+            if not guild:
+                return ["I cannot accept reports of messages from guilds that I'm not in. Please have the guild owner add me to the guild and try again."]
+            channel = guild.get_channel(int(m.group(2)))
+            if not channel:
+                return ["It seems this channel was deleted or never existed. Please try again or say `done` to proceed with finishing the report."]
+            try:
+                add_msg = await channel.fetch_message(int(m.group(3)))
+            except discord.errors.NotFound:
+                return ["It seems this message was deleted or never existed. Please try again or say `done` to proceed with finishing the report."]
+            if add_msg.author.id != self.reported_author_id:
+                return ["This message was not sent by the offender. Please try again or say `done` to proceed with finishing the report."]
+            self.other_message_chain = (self.other_message_chain + 'cs152' + add_msg.content) 
+            return ["I have added the message to the report where each offending message is delimited by 'cs152' as follows: " + "```" + self.other_message_chain + "``` \n" + "Please reply with another link to a message from the same offender, or say `done` to proceed with finishing the report. \n"]
+
         if self.state == State.RECEIVED_SPAM_TYPE:
             report_reply = ''
             if message.content not in ['yes', 'no']:
