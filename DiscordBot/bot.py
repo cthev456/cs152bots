b056e66ff839ca61aa8491e23aec8da7aeed1928
Ziang Liu <ziang_liu@outlook.com>
2023-06-03 14:25:33 -0700

    Move mod flow to mod channel

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -80,7 +80,10 @@ class ModBot(discord.Client):
 
         # Check if this message was sent in a server ("guild") or if it's a DM
         if message.guild:
-            await self.handle_channel_message(message)
+            if "mod" in message.channel.name:
+                await self.handle_mod_channel_message(message)
+            else:
+                await self.handle_channel_message(message)
         else:
             await self.handle_dm(message)
 
@@ -154,26 +157,27 @@ class ModBot(discord.Client):
 
     async def handle_mod_flow(self, message):
         author_id = message.author.id
+        mod_channel = list(self.mod_channels.values())[0]
 
         if author_id not in self.moderation_actions:
             self.moderation_actions[author_id] = None
-            await message.channel.send('Please say the id of the report to moderate')
+            await mod_channel.send('Please say the id of the report to moderate')
             return
 
         if not self.moderation_actions[author_id]:
-            await message.channel.send('Thank you. Finding that report now')
+            await mod_channel.send('Thank you. Finding that report now')
             report_id = int(message.content)
             for report in self.reports.values():
                 if report.id == report_id:
                     if report.state == State.AWAITING_MOD:
                         self.moderation_actions[author_id] = report
-                        await message.channel.send('I found the report with this message:' + "```" + report.message.author.name + ": " + report.message.content + "``` \n")
+                        await mod_channel.send('I found the report with this message:' + "```" + report.message.author.name + ": " + report.message.content + "``` \n")
                         report.eval_type = self.eval_text(report.message.content)
-                        await message.channel.send(f'The autoclassifier thinks this is a violation of type {report.eval_type}. Is this correct?')
+                        await mod_channel.send(f'The autoclassifier thinks this is a violation of type {report.eval_type}. Is this correct?')
                         report.state = State.AWAITING_MOD_CONFIRM
                         return
                     else:
-                        await message.channel.send('It appears someone else is already moderating this message.')
+                        await mod_channel.send('It appears someone else is already moderating this message.')
                     break
 
         report = self.moderation_actions[author_id]
@@ -195,7 +199,7 @@ class ModBot(discord.Client):
                 response += "5. Hate Speech; type 'hate speech' \n"
                 response += "6. Other; type 'other' \n"
                 response += "7. None; type 'unidentified'"
-                await message.channel.send(response)
+                await mod_channel.send(response)
                 return
 
         if report.state == State.AWAITING_MOD_CLASSIFICATION:
@@ -208,7 +212,7 @@ class ModBot(discord.Client):
                 report_reply += "5. Hate Speech; type 'hate speech' \n"
                 report_reply += "6. Other; type 'other' \n"
                 report_reply += "7. None; type 'unidentified'"
-                await message.channel.send(report_reply)
+                await mod_channel.send(report_reply)
                 return
             if message.content == Category.SPAM: # spam
                 report.state = State.AWAITING_MOD_SUBCLASSIFICATION
@@ -217,19 +221,19 @@ class ModBot(discord.Client):
                     + "2. Unwanted invites to other servers; type 'invites' \n" \
                     + "3. The message contains a suspicious, abusive, or NSFW link; type 'links' \n" \
                     + "4. Other; type 'other' \n"
-                await message.channel.send(report_reply)
+                await mod_channel.send(report_reply)
                 return
 
             else:
                 if message.content == 'unidentified':
                     report.eval_type = 'unidentified'
                     report.state = State.MOD_COMPLETE
-                    await message.channel.send("Thank you")
+                    await mod_channel.send("Thank you")
                     return
                 response = "Is the violation minor or severe?"
                 report.eval_type = 'violation_' + message.content
                 report.state = State.AWAITING_MOD_SEVERITY
-                await message.channel.send(response)
+                await mod_channel.send(response)
                 return
 
         if report.state == State.AWAITING_MOD_SUBCLASSIFICATION:
@@ -239,21 +243,21 @@ class ModBot(discord.Client):
                 report_reply += "2. Unwanted invites to other servers; type 'invites' \n"
                 report_reply += "3. The message contains a suspicious, abusive, or NSFW link; type 'links' \n"
                 report_reply += "4. Other; type 'other' \n"
-                await message.channel.send(report_reply)
+                await mod_channel.send(report_reply)
                 return
             report.eval_type += '_' + message.content
             report.state = State.AWAITING_MOD_SEVERITY
             response = "Is the violation minor or severe?"
-            await message.channel.send(response)
+            await mod_channel.send(response)
             return
 
         if report.state == State.AWAITING_MOD_SEVERITY:
             if message.content not in ['minor', 'severe']:
                 report_reply = 'Please state either minor or severe'
-                await message.channel.send(report_reply)
+                await mod_channel.send(report_reply)
                 return
             report.eval_type += '_' + message.content
-            await message.channel.send("Thank you. Finalizing evaluation.")
+            await mod_channel.send("Thank you. Finalizing evaluation.")
             await self.handle_moderation(report, report.eval_type)
 
         # If the report is complete or cancelled, remove it from our map
@@ -273,10 +277,6 @@ class ModBot(discord.Client):
 
         author_id = message.author.id
         responses = []
-
-        if author_id in self.moderation_actions or message.content.startswith(Report.MOD_KEYWORD):
-            await self.handle_mod_flow(message)
-            return
 
         # Only respond to messages if they're part of a reporting flow
         if author_id not in self.reports and not message.content.startswith(Report.START_KEYWORD):
@@ -322,6 +322,18 @@ class ModBot(discord.Client):
         scores = self.eval_text(message.content)
         await mod_channel.send(self.code_format(scores))
 
+    async def handle_mod_channel_message(self, message):
+        # Only handle messages sent in the "group-#" channel
+        if not message.channel.name == f'group-{self.group_num}-mod':
+            return
+
+        # Forward the message to the mod channel
+        mod_channel = self.mod_channels[message.guild.id]
+        
+        author_id = message.author.id
+        if author_id in self.moderation_actions or message.content.startswith(Report.MOD_KEYWORD):
+            await self.handle_mod_flow(message)
+            return
     
     def eval_text(self, message):
         ''''
